bb7b5d4744e04b05004e2796622710fa21e5357d
Shishak <semenshishak@gmail.com>
2020-02-03 10:53:11 +0200

    Update WordPressPost.vue

--- src/templates/WordPressPost.vue
+++ src/templates/WordPressPost.vue
@@ -1,23 +1,28 @@
 <template>
   <Layout>
     <h1 v-html="$page.wordPressPost.title" />
+    <!-- <img
+      v-if="$page.wordPressPost.featuredMedia"
+      :width="$page.wordPressPost.featuredMedia.mediaDetails.width"
+      :alt="$page.wordPressPost.featuredMedia.altText"
+    />-->
     <div v-html="$page.wordPressPost.content" />
-     <template v-if="$page.wordPressPost.categories.length">
+    <!-- <template v-if="$page.wordPressPost.categories.length">
       <h4>Posted in</h4>
       <ul class="list categories">
         <li v-for="category in $page.wordPressPost.categories" :key="category.id">
           <g-link :to="category.path">{{ category.title }}</g-link>
         </li>
       </ul>
-    </template>
-    <template v-if="$page.wordPressPost.tags.length">
+    </template>-->
+    <!-- <template v-if="$page.wordPressPost.tags.length">
       <h4>Tags</h4>
       <ul class="list tags">
         <li v-for="tag in $page.wordPressPost.tags" :key="tag.id">
           <g-link :to="tag.path">{{ tag.title }}</g-link>
         </li>
       </ul>
-    </template>
+    </template>-->
   </Layout>
 </template>
 
